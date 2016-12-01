f9a8ac09fe4f05d4d736cd8b3b259c09e3f55358
Pontus <Pontus@192.168.0.160>
2016-12-01 16:02:02 +0100

    Structure

---
+++ src/main/java/se/pjab/config/JerseyConfig.java
@@ -0,0 +1,11 @@
+package se.pjab.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public final class JerseyConfig extends ResourceConfig {
+
+	public JerseyConfig() {
+		
+	}
+
+}
---
+++ src/main/java/se/pjab/model/TypeText.java
@@ -0,0 +1,28 @@
+package se.pjab.model;
+
+public class TypeText {
+
+	private long id;
+	private String content;
+	
+	public TypeText() {
+		
+	}
+
+	public TypeText(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+}
